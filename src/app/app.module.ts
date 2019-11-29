5bc60fdf939110b242fccda4826e6018ffc95c9e
hemankur <hemankur@hotmail.com>
2019-11-28 19:21:50 -0500

    Socket.io

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,22 @@ import {FormsModule} from "@angular/forms";
 import {LoginComponent} from './login/login.component';
 import {HttpClientModule} from "@angular/common/http";
 import {RegistrationComponent} from './registration/registration.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { LobbyComponent } from './lobby/lobby.component';
-import { MenuComponent } from './menu/menu.component';
-import { RulesComponent } from './rules/rules.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {LobbyComponent} from './lobby/lobby.component';
+import {MenuComponent} from './menu/menu.component';
+import {RulesComponent} from './rules/rules.component';
+import {SocketIoConfig, SocketIoModule} from "ngx-socket-io";
+import {SERVER_URL} from "../environments/environment";
+
+const config: SocketIoConfig = {
+  url: SERVER_URL, options: {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    withCredentials: true
+  }
+};
 
 @NgModule({
   declarations: [
@@ -26,6 +38,7 @@ import { RulesComponent } from './rules/rules.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    SocketIoModule.forRoot(config),
     HttpClientModule
   ],
   providers: [],
--- src/app/lobby/lobby.component.ts
+++ src/app/lobby/lobby.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "../api/user.service";
+import {LobbyService} from "../api/lobby.service";
+import {Socket} from "ngx-socket-io";
+import {WebSocketService} from "../api/web-socket.service";
 
 @Component({
   selector: 'app-lobby',
@@ -9,15 +12,21 @@ import {UserService} from "../api/user.service";
 export class LobbyComponent implements OnInit {
   username: string;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService,
+              private lobbyService: LobbyService,
+              private webSocketService: WebSocketService) {
   }
 
   ngOnInit() {
-    this.username = 'hemankur';
+    this.username = localStorage.getItem('username');
     this.userService.getUserData(this.username)
       .then(res => {
         console.log(res);
       });
+
+    this.webSocketService.listen("test event").subscribe((data) => {
+      console.log(data);
+    });
   }
 
 }
